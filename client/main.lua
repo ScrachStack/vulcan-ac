b62f89a7afe3166b846fce0233ff0a83513e4d4a
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-13 08:26:16 -0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,1 +1,17 @@
 -- join https://discord.gg/cfxdev for support
+if Config.BlacklistedMenuTextures.Enabled then
+    CreateThread(function()
+        while true do
+            Citizen.Wait(3000) 
+
+            for _, textureInfo in ipairs(Config.BlacklistedMenuTextures.Textures) do
+                if HasStreamedTextureDictLoaded(textureInfo.Texture) then
+                        TriggerServerEvent('zaps:kick',  Config.BlacklistedMenuTextures.Message .. " (" .. textureInfo.Label .. ")")
+                        if Config.DebugMode then 
+                        print(textureInfo.Label)
+                        end
+                end
+            end
+        end
+    end)
+end
