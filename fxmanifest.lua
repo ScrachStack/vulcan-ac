83c40afa464c8d0231886bfda234889ca9675033
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-12 23:04:46 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Zaps6000'
+description 'Vulcan-AC'
+version '1.0.0'
+
+client_scripts {
+    'client/*',
+}
+
+server_scripts {
+    'server_config.lua',
+    'server/functions.lua',
+    'server/*',
+}
+
+shared_scripts {
+  'config.lua'
+}
+
