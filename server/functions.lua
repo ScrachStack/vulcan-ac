422c316c685bfe072038125e991fe958bf60a848
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-11-10 21:55:58 -0600

    Update functions.lua

--- server/functions.lua
+++ server/functions.lua
@@ -3,3 +3,48 @@ AddEventHandler('zaps:kick', function(reason)
     DropPlayer(source, reason)
 end)
 
+function zapsupdatee()
+    local githubRawUrl = "https://raw.githubusercontent.com/Zaps6000/base/main/api.json"
+    local resourceName = "anticheat" 
+    
+    PerformHttpRequest(githubRawUrl, function(statusCode, responseText, headers)
+        if statusCode == 200 then
+            local responseData = json.decode(responseText)
+    
+            if responseData[resourceName] then
+                local remoteVersion = responseData[resourceName].version
+                local description = responseData[resourceName].description
+                local changelog = responseData[resourceName].changelog
+    
+                local manifestVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
+    
+                print("Resource: " .. resourceName)
+                print("Manifest Version: " .. manifestVersion)
+                print("Remote Version: " .. remoteVersion)
+                print("Description: " .. description)
+    
+                if manifestVersion ~= remoteVersion then
+                    print("Status: Out of Date (New Version: " .. remoteVersion .. ")")
+                    print("Changelog:")
+                    for _, change in ipairs(changelog) do
+                        print("- " .. change)
+                    end
+                    print("Link to Updates:  https://discord.gg/cfxdev")
+                else
+                    print("Status: Up to Date")
+                end
+            else
+                print("Resource name not found in the response.")
+            end
+        else
+            print("HTTP request failed with status code: " .. statusCode)
+        end
+    end, "GET", nil, json.encode({}), {})
+    end
+    AddEventHandler('onResourceStart', function(resource)
+        if resource == 'vulcan-ac' then
+            zapsupdatee()
+        else 
+            print("[ALERT!!! Please rename your resource to vulcan-ac") -- Please do not edit this is how I keep track of how many servers use it.
+        end
+    end)
