6ea27b97792cf0b5629155405f942bc6dd1d555b
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-11-11 01:41:46 -0600

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,5 @@
 -- join https://discord.gg/cfxdev for support
+local Loaded <const> = false
 if Config.BlacklistedMenuTextures.Enabled then
     CreateThread(function()
         while true do
@@ -33,11 +34,37 @@ if Config.BlacklistedEvents.Enabled then
     for _, event in pairs(Config.BlacklistedEvents.Events) do
         if event.type == 'client' then
             AddEventHandler(event.Name, function()
-                if not hasBypassPermission(source, Config.BlacklistedEvents.ACEPermission) then
+            --    if not hasBypassPermission(source, Config.BlacklistedEvents.ACEPermission) then need to finish said function
                     CancelEvent()
                     TriggerServerEvent('zaps:kick', Config.BlacklistedEvents.Message)
-                end
+              --  end
             end)
         end
     end
+end
+AddEventHandler('playerSpawned', function(spawn)
+	Loaded = true
+end)
+if Config.SuperJump.Enabled then
+if Loaded then 
+CreateThread(function()
+    while true do
+        Wait(1500)
+        if IsPedJumping(PlayerPedId()) then
+            local firstCoord = GetEntityCoords(PlayerPedId())
+            while IsPedJumping(PlayerPedId()) do
+                Wait(0)
+            end
+            local secondCoord = GetEntityCoords(PlayerPedId())
+            local Length = GetDistanceBetweenCoords(firstCoord, secondCoord, false)
+            if Config.SuperJump.LengthThreshold > 15 then -
+                        if Config.Debug then 
+                            print('Detected  jump: Length = ' .. jumplength)
+                        end
+            TriggerServerEvent('zaps:kick', Config.SuperJump.Message)
+            end
+        end
+    end
+end)
+end
 end
