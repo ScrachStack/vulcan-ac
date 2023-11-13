085c1075e72ebbe91c7f9da50fb2c0a9125c62c1
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-11-12 18:01:05 -0600

    Update functions.lua

--- server/functions.lua
+++ server/functions.lua
@@ -19,29 +19,43 @@ RegisterCommand('kick', function(source, args, rawCommand)
         end
     end, false)
 end
-RegisterNetEvent('logKickToDiscordEvent')
-AddEventHandler('logKickToDiscordEvent', function(name, reason)
-    TriggerEvent('sendScreenshotToServer')
-    local discordWebhookUrl = GetConvar("vac:webhook", "")
-
-    if discordWebhookUrl == "" or discordWebhookUrl == "https://discord.com/api/webhooks/" then
-        print("[VulcanAC]: Edit webhook.cfg Or Add this to your server.cfg @vulcan-ac/webhook.cfg")
-        return
-    end
-
-    local discordMessage = {
-        username = "Vulcan Anticheat", 
-        embeds = {{
-            title = "Suspected Cheater Kicked",
-            description = "Player **" .. name .. "** was kicked for suspected cheating.\n**Reason:** " .. reason,
-            color = 16711680,
-        }}
-    }
-
-    PerformHttpRequest(discordWebhookUrl, function(err, text, headers)
-    end, 'POST', json.encode(discordMessage), { ['Content-Type'] = 'application/json' })
-end)
-
+    RegisterNetEvent('logKickToDiscordEvent')
+    AddEventHandler('logKickToDiscordEvent', function(name, reason)
+        local discordWebhookUrl = GetConvar("vac:webhook", "")
+        if discordWebhookUrl == "" or discordWebhookUrl == "https://discord.com/api/webhooks/" then
+            print("[VulcanAC]: Edit webhook.cfg Or Add this to your server.cfg @vulcan-ac/webhook.cfg")
+            return
+        end
+    
+        local identifiers = GetPlayerIdentifiers(name)
+        local playerSteamID, playerLicense, playerIP = "", "", ""
+    
+        for _, identifier in ipairs(identifiers) do
+            if string.find(identifier, "steam") then
+                playerSteamID = identifier
+            elseif string.find(identifier, "license") then
+                playerLicense = identifier
+            elseif string.find(identifier, "ip") then
+                playerIP = identifier
+            end
+        end
+    
+        local discordMessage = {
+            username = "Vulcan Anticheat", 
+            embeds = {{
+                title = "Suspected Cheater Kicked",
+                description = "Player **" .. GetPlayerName(name) .. "** with identifiers:"
+                    .. (Config.Moderation.Webhook['webhook'].SteamHex and "\n- **Steam ID:** " .. playerSteamID or "")
+                    .. (Config.Moderation.Webhook['webhook'].License and "\n- **License:** " .. playerLicense or "")
+                    .. (Config.Moderation.Webhook['webhook'].IP and "\n- **IP:** " .. playerIP or "")
+                    .. "\nwas kicked for suspected cheating.\n**Reason:** " .. reason,
+                color = 16711680,
+            }}
+        }
+    
+        PerformHttpRequest(discordWebhookUrl, function(err, text, headers)
+        end, 'POST', json.encode(discordMessage), { ['Content-Type'] = 'application/json' })
+    end)
 
 function zapsupdatee()
     local githubRawUrl = "https://raw.githubusercontent.com/Zaps6000/base/main/api.json"
