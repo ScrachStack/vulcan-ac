ea6c8b3beb4d0b8ed29ebf7919160a6ab82acba8
ScratchStack <122732007+ScrachStack@users.noreply.github.com>
2025-11-16 23:09:59 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,23 @@
--- for support https://discord.gg/cfxdev
+-- for support https://syncstudio.org/discord 
 fx_version 'cerulean'
 games {  'gta5', 'rdr3' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
--- Can remove warning if only on fivem and not using redm
 lua54 'yes'
-author 'Zaps6000'
-description 'Vulcan-AC'
+author 'ScratchStack (Sync Studio)'
+description ' '
 version '1.5.9'
+name 'vulcan-ac'
+license 'GNU GENERAL PUBLIC LICENSE'
+repository 'https://github.com/ScrachStack/vulcan-ac'
 
 client_scripts {
     'client/*',
 }
 
 server_scripts {
-    'server/functions.lua',
+    'index.js',
     'server/*',
+    
 }
 
 shared_scripts {
